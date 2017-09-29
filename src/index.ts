c3ee0d450a7227075ad566a9f71c46e4a7365935
nin <ninian138@gmail.com>
2017-09-29 18:35:52 +0900

    add parser

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 import {readFile} from "./readFile";
-import * as fs from "fs-extra";
+// import * as fs from "fs-extra";
 import {Parser} from "./parser";
 
 export const current_dir = process.cwd() + "/";
-export const out_dir = `${process.cwd()}/ninit`;
+export const out_dir = `${process.cwd()}/ninit/`;
 
 readFile("nin/index.nin")
     .then((text: string) => onReadIndex(text))
@@ -13,6 +13,6 @@ const onReadIndex = (text: string) => {
   const parser = new Parser(text);
   return parser.parse();
 };
-fs.removeSync(out_dir);
-fs.mkdirSync(out_dir);
-fs.copySync(current_dir + "template/index.html", out_dir);
+// fs.removeSync(out_dir);
+// fs.mkdirSync(out_dir);
+// fs.copySync(current_dir + "template/index.html", out_dir + "index.html");
--- src/parser.ts
+++ src/parser.ts
@@ -1,4 +1,31 @@
 export class Parser {
-  constructor(text: string) {}
-  parse() {}
+  private index = 0;
+  __text: string;
+  // private lines = [];
+  constructor(private text: string) { this.__text = text; }
+  parse() {
+    this.toLines();
+  }
+  private getLineNumber(index: number) {
+    return this.__text.substr(0, index).split("").filter(c => c == "\n").length + 1
+  }
+  private toLines() {
+    const key = this.readTo(/:/).line.trim();
+    if(key.includes("\n") || key.includes(" ")) {
+      console.log(`key is incorrect at line: ${this.getLineNumber(this.index)}`);
+      return;
+    }
+    console.log(key);
+  }
+  private readTo(reg: RegExp) {
+    let line = "";
+    while(!reg.test(this.text[this.index])) {
+      line += this.text[this.index];
+      this.index++;
+    }
+    const match = this.text[this.index];
+    this.index++;
+    return { line, match };
+  }
+
 }
