68009dfa81109ed6c493f8bf767aebd0217a356c
nin <ninian138@gmail.com>
2017-11-28 11:17:39 +0900

    change toml

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,16 @@
-import {readFile} from "./readFile";
 // import * as fs from "fs-extra";
-import * as Toml from "toml";
 // const Toml = require('toml-js');
+import {readIndex} from "./transpiler";
+
 export const current_dir = process.cwd() + "/";
 export const out_dir = `${process.cwd()}/ninit/`;
 
 // readFile("nin/index.nin")
-readFile("toml/src/app.toml")
-    .then((text: string) => console.log(Toml.parse(text)))
-    .catch(e => { throw e });
+// readFile("toml/src/app.toml")
+//     .then((text: string) => console.log(Toml.parse(text)))
+//     .catch(e => { throw e });
+readIndex().then(it => console.log(it));
+
 
 // fs.removeSync(out_dir);
 // fs.mkdirSync(out_dir);
