ff33b2cc2acf88ed2e5f233ea1bf17014dce5ada
nin <ninian138@gmail.com>
2017-11-29 17:06:02 +0900

    change util.Toml.parse return type

--- src/util.ts
+++ src/util.ts
@@ -2,7 +2,7 @@ import * as bombadil from "@sgarciac/bombadil"
 const tomlify = require('tomlify-j0.4');
 
 export const Toml = {
-  parse: (toml: string): object => {
+  parse: (toml: string): any => {
     const reader = new bombadil.TomlReader;
     reader.readToml(toml);
     return reader.result;
