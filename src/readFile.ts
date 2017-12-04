b6040e316d7abf1ce0f400382b5790947a8ce254
nin <ninian138@gmail.com>
2017-12-04 15:57:35 +0900

    change readFile return value to Promise

--- src/readFile.ts
+++ src/readFile.ts
@@ -3,7 +3,7 @@ import * as fs from "fs";
 export const readFile = (path: string): Promise<string> => {
   return new Promise(((resolve, reject) => {
     const encode: string = "utf8";
-    fs.readFile(`${process.cwd()}/${path}`,encode, (err: NodeJS.ErrnoException, data: string) => {
+    fs.readFile(path, encode, (err: NodeJS.ErrnoException, data: string) => {
       if(err) reject(err);
       resolve(data);
     });
